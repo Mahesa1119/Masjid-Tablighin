aaa65604e4c910bf6b3613c1699238e90e520917
Mahesa1119 <raihanmahesa830@gmail.com>
2024-11-24 19:46:32 +0700

    updatee

---
+++ .history/script_20241124185956.js
@@ -0,0 +1,12 @@
+const menuIcon = document.getElementById("hamburger");
+const menuList = document.querySelector(".nav-list");
+    
+    menuIcon.addEventListener("click", () => {
+        menuList.classList.toggle("active");
+    });
+
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('header');
+    header.classList.toggle('scrolled', window.scrollY > 50);
+});
+
